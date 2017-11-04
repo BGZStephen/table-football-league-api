3956e8ce988e3dbfcfab02aa5a167c1832b2fafe
Stephen Wright <sjw948@gmail.com>
2017-11-04 18:27:44 +0000

    config sample file setup

---
+++ node/api/app.js
@@ -0,0 +1,1 @@
+global.ENV = process.env.NODE_ENV || 'development';
---
+++ node/api/config/sample-config.js
@@ -0,0 +1,19 @@
+const configs = {
+  development: {
+    database: '$DATABASE_URL',
+    authorization: '$AUTHORIZATION',
+    jwtSecret: '$JWT_SECRET',
+  }
+  staging: {
+    database: '$DATABASE_URL',
+    authorization: '$AUTHORIZATION',
+    jwtSecret: '$JWT_SECRET',
+  }
+  production: {
+    database: '$DATABASE_URL',
+    authorization: '$AUTHORIZATION',
+    jwtSecret: '$JWT_SECRET',
+  }
+}
+
+module.exports = configs[Global.ENV]
