cc1907a1f46ee0643ca341991b946b14fe9020d4
Stephen Wright <sjw948@gmail.com>
2017-11-06 20:47:40 +0000

    league create + league getAll

--- node/api/routes/public/index.js
+++ node/api/routes/public/index.js
@@ -7,6 +7,9 @@ router.get('/users/:id', Users.getOne);
 router.post('/users', Users.create);
 router.post('/users/authenticate', Users.authenticate);
 
+router.get('/leagues', Leagues.getAll);
 router.post('/leagues', Leagues.create);
+router.all('/leagues/id', Leagues.fetchLeague)
+router.get('/leagues/id', Leagues.getOne);
 
 module.exports = router;
--- node/api/routes/public/leagues.js
+++ node/api/routes/public/leagues.js
@@ -8,10 +8,26 @@ async function create(req, res) {
       createdOn: Date.now(),
       name: req.body.name,
       administrators: [req.body.userId],
-    })
+    });
 
     await league.save();
     res.json(league);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}
+
+async function getOne(req, res) {
+  if(!req.league) {
+    return res.status(404).json({message: 'League not found'});
+  }
+  res.json(req.league);
+}
+
+async function getAll(req, res) {
+  try {
+    const leagues = await League.find({});
+    res.json(leagues);
   } catch (error) {
     res.status(500).json(error);
   }
