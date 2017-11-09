869bdc09ea958adf8b740ef5154308ce071baa68
Stephen Wright <sjw948@gmail.com>
2017-11-09 09:06:19 +0000

    add nodemail, cloudinary + basic email setup

---
+++ node/api/services/mailer/index.js
@@ -0,0 +1,64 @@
+const Promise = require('bluebird');
+const config = require('../../config');
+const ejs = require('ejs');
+const validate = require('validate.js');
+const mailjet = require('node-mailjet').connect(config.mailJet.apiKey, config.mailJet.apiSecret, {
+	url: 'api.mailjet.com',
+	version: 'v3.1',
+});
+
+const ejsRenderFile = Promise.promisify(ejs.renderFile);
+
+async function sendEmail(params) {
+
+	if (!params.from) {
+		throw new Error('From is a required parameter');
+	}
+
+	if (!params.to) {
+		throw new Error('To is a required parameter');
+	}
+
+	if (!params.subject) {
+		throw new Error('Subject is a required parameter');
+	}
+
+	if (!params.template) {
+		throw new Error('HTML template name is required');
+	}
+
+  const emailParams = {
+    'Messages':[{
+      'From': {
+        'Email': `${params.from}`,
+        'Name': `${params.fromName ? params.fromName : ''}`
+      },
+      'To': [{
+        'Email': `${params.to}`,
+        'Name': `${params.toName ? params.toName : ''}`
+      }],
+      'Subject': `${params.subject}`,
+      'TextPart': `${params.textPart ? params.textPart : ''}`,
+      'HTMLPart': await ejsRenderFile(params.template),
+    }]
+  };
+
+	await mailjet.post('send').request(emailParams);
+	return {success: true};
+}
+
+async function welcomeEmail(params) {
+	validate(params, {
+		from: {presence: true},
+		to: {presence: true},
+		subject: {presence: true},
+		template: {presence: true},
+		user: {presence: true}
+	});
+
+	return await sendEmail(params);
+}
+
+module.exports = {
+	welcomeEmail,
+};
