f6547779927b97b40fab20d3d01ba62a34eeb5e4
Stephen Wright <sjw948@gmail.com>
2017-11-11 20:32:46 +0000

    website-home full screen view setup

--- angular/src/app/modules/website/components/website-home/website-home.component.ts
+++ angular/src/app/modules/website/components/website-home/website-home.component.ts
@@ -3,8 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-website-home',
   templateUrl: './website-home.component.html',
-  styleUrls: ['./website-home.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class WebsiteHomeComponent implements OnInit {
 
