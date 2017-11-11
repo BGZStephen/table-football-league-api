c3f9a93cf85c1d83d2c026a90b5a7458528e8e6f
Stephen Wright <sjw948@gmail.com>
2017-11-11 15:45:47 +0000

    responsive dashboard navbar + small css fix for icon indentation on large screens

--- angular/src/app/modules/dashboard/components/dashboard-view-wrapper/dashboard-view-wrapper.component.ts
+++ angular/src/app/modules/dashboard/components/dashboard-view-wrapper/dashboard-view-wrapper.component.ts
@@ -3,8 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-dashboard-view-wrapper',
   templateUrl: './dashboard-view-wrapper.component.html',
-  styleUrls: ['./dashboard-view-wrapper.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class DashboardViewWrapperComponent implements OnInit {
 
--- angular/src/app/modules/website/components/website-view-wrapper/website-view-wrapper.component.ts
+++ angular/src/app/modules/website/components/website-view-wrapper/website-view-wrapper.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-website-view-wrapper',
   templateUrl: './website-view-wrapper.component.html',
-  encapsulation: ViewEncapsulation.None
 })
 export class WebsiteViewWrapperComponent implements OnInit {
 
