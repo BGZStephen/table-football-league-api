bb4af2ca8e8b0e55874415a1db7388fb8d11fb80
Stephen Wright <sjw948@gmail.com>
2017-11-11 13:11:45 +0000

    website navbar styles (full-screen)

--- angular/src/app/modules/website/components/website-navbar/website-navbar.component.ts
+++ angular/src/app/modules/website/components/website-navbar/website-navbar.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-website-navbar',
   templateUrl: './website-navbar.component.html',
-  styleUrls: ['./website-navbar.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class WebsiteNavbarComponent implements OnInit {
