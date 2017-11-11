231c8bb657b3f1a40d9a92f680e6ee4b82ff0b2b
Stephen Wright <sjw948@gmail.com>
2017-11-11 15:40:07 +0000

    responsive website menu

--- angular/src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
+++ angular/src/app/modules/dashboard/components/dashboard-navbar/dashboard-navbar.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-dashboard-navbar',
   templateUrl: './dashboard-navbar.component.html',
-  styleUrls: ['./dashboard-navbar.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class DashboardNavbarComponent implements OnInit {
