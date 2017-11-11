1af0dd4bfe2c2e27490efdfca4aa830f866e82f0
Stephen Wright <sjw948@gmail.com>
2017-11-11 12:16:41 +0000

    media query vars + website-view-wrapper base layout styles

--- angular/src/app/modules/website/components/website-view-wrapper/website-view-wrapper.component.ts
+++ angular/src/app/modules/website/components/website-view-wrapper/website-view-wrapper.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-website-view-wrapper',
   templateUrl: './website-view-wrapper.component.html',
-  styleUrls: ['./website-view-wrapper.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
 export class WebsiteViewWrapperComponent implements OnInit {
