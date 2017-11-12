2e277df8b94f8997e2aee2d257c3fc04c4ee0278
Stephen Wright <sjw948@gmail.com>
2017-11-12 15:12:27 +0000

    added form validation for user registration

--- angular/src/app/modules/website/components/website-register/website-register.component.ts
+++ angular/src/app/modules/website/components/website-register/website-register.component.ts
@@ -6,9 +6,62 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class WebsiteRegisterComponent implements OnInit {
 
+  formValues = {
+    firstName: {
+      display: 'First name'
+    },
+    lastName: {
+      display: 'Last name'
+    },
+    username: {
+      display: 'Username'
+    },
+    email: {
+      display: 'Email'
+    },
+    password: {
+      display: 'Password'
+    },
+    confirmPassword: {
+      display: 'Password confirmation'
+    },
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onRegister(form) {
+    const validation = this.validateForm(form);
+    if (validation) {
+      // create user
+    }
+  }
+
+  validateForm(form) {
+    let errorFlag;
+    Object.keys(this.formValues).forEach((key) => {
+      if(!form[key]) {
+        errorFlag = true;
+        this.formValues[key]['hasError'] = true;
+        this.formValues[key]['message'] = `${this.formValues[key].display} is required`;
+      } else {
+        this.formValues[key]['hasError'] = false;
+        this.formValues[key]['message'] = undefined;
+      }
+    })
+
+    if (form.confirmPassword !== form.password) {
+      errorFlag = true;
+      this.formValues.confirmPassword['hasError'] = true;
+      this.formValues.confirmPassword['message'] = 'The entered passwords do not match';
+    }
+
+    if(errorFlag) {
+      return false;
+    }
+
+    return true;
+  }
 }
