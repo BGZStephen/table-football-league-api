773e4304bfe049bec95a7d5468dd3527e9816d4b
Stephen Wright <sjw948@gmail.com>
2017-11-12 09:24:05 +0000

    responsive collapsable dashboard + website navbar

--- angular/src/app/modules/website/components/website-navbar/website-navbar.component.ts
+++ angular/src/app/modules/website/components/website-navbar/website-navbar.component.ts
@@ -7,9 +7,27 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class WebsiteNavbarComponent implements OnInit {
 
+  menuVisible = false;
+
   constructor() { }
 
   ngOnInit() {
+
   }
 
+  toggleMenuVisible() {
+    this.menuVisible = !this.menuVisible;
+  }
+
+  menuVisibleStyle() {
+    if (screen.width > 1024) {
+      this.menuVisible = true;
+      return {'max-height': 'calc(100vh - 114px)'}
+    } else if (this.menuVisible) {
+      const height = `${document.getElementById('website-navbar').getElementsByTagName('ul')[0].children.length * 52}px`
+      return {'max-height': `${height}`};
+    } else {
+      return {'max-height': '0'};
+    }
+  }
 }
