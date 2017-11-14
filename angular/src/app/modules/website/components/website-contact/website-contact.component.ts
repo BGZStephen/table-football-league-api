39b46b3e861f3a8f189a6643dfdaf128b9e4a520
Stephen Wright <sjw948@gmail.com>
2017-11-14 13:13:16 +0000

    website-contact template

--- angular/src/app/modules/website/components/website-contact/website-contact.component.ts
+++ angular/src/app/modules/website/components/website-contact/website-contact.component.ts
@@ -3,8 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-website-contact',
   templateUrl: './website-contact.component.html',
-  styleUrls: ['./website-contact.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class WebsiteContactComponent implements OnInit {
 
