b36f0e197702c66e99954a263c1843d3cdbf1dff
Stephen Wright <sjw948@gmail.com>
2017-11-14 08:58:42 +0000

    website-login component design + linked to api

--- angular/src/app/modules/website/components/website-login/website-login.component.ts
+++ angular/src/app/modules/website/components/website-login/website-login.component.ts
@@ -1,16 +1,63 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ApiService } from 'app/services/api.service';
+import { GlobalService } from 'app/services/global.service';
 
 @Component({
   selector: 'app-website-login',
   templateUrl: './website-login.component.html',
-  styleUrls: ['./website-login.component.scss'],
-  encapsulation: ViewEncapsulation.None
 })
 export class WebsiteLoginComponent implements OnInit {
 
-  constructor() { }
+  formValues = {
+    email: {
+      display: 'Email'
+    },
+    password: {
+      display: 'Password'
+    },
+  };
+
+  constructor(
+    private api: ApiService,
+    private globalService: GlobalService,
+  ) { }
 
   ngOnInit() {
+  }
+
+  onLogin(user) {
+    const validation = this.validateForm(user);
+    if (validation) {
+      this.api.users.authenticate(user)
+      .subscribe(
+        user => {
+          this.globalService.notification.show({message: 'Login successful'});
+        },
+        errorRes => {
+          this.globalService.notification.error({message: errorRes.error});
+        }
+      )
+    }
+  }
+
+  validateForm(form) {
+    let errorFlag;
+    Object.keys(this.formValues).forEach((key) => {
+      if(!form[key]) {
+        errorFlag = true;
+        this.formValues[key]['hasError'] = true;
+        this.formValues[key]['message'] = `${this.formValues[key].display} is required`;
+      } else {
+        this.formValues[key]['hasError'] = false;
+        this.formValues[key]['message'] = undefined;
+      }
+    })
+
+    if(errorFlag) {
+      return false;
+    }
+
+    return true;
   }
 
 }
