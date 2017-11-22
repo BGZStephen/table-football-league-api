61fc87b27dac0a02e1ed555c5ea53d9dd7dae4f1
Stephen Wright <sjw948@gmail.com>
2017-11-22 08:38:15 +0000

    fixed dashboard + website navbar

--- angular/src/app/modules/website/components/website-navbar/website-navbar.component.ts
+++ angular/src/app/modules/website/components/website-navbar/website-navbar.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 @Component({
   selector: 'app-website-navbar',
   templateUrl: './website-navbar.component.html',
-  encapsulation: ViewEncapsulation.None
 })
 export class WebsiteNavbarComponent implements OnInit {
 
