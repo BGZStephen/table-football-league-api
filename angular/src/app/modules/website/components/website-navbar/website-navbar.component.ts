553533d0b9e18466596858b4e41c91c02b8c13e5
Stephen Wright <sjw948@gmail.com>
2017-11-26 10:10:25 +0000

    moved to iterable menu builder for automated submenu indexing

--- angular/src/app/modules/website/components/website-navbar/website-navbar.component.ts
+++ angular/src/app/modules/website/components/website-navbar/website-navbar.component.ts
@@ -6,8 +6,13 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class WebsiteNavbarComponent implements OnInit {
 
-  menuVisible: boolean = false;
   currentSubmenuActive: number = -1;
+  menuVisible: boolean = false;
+  menuItems: Array<object> = [
+    {name: 'Home', iconClass: 'fa fa-home fa-2x', link: '/'},
+    {name: 'Register', iconClass: 'fa fa-pencil fa-2x', link: '/register'},
+    {name: 'Login', iconClass: 'fa fa-sign-in fa-2x', link: '/login'},
+  ]
 
   constructor() { }
 
@@ -22,7 +27,7 @@ export class WebsiteNavbarComponent implements OnInit {
   }
 
   submenuActiveStyle(index) {
-    if (index === this.currentSubmenuActive) {
+    if (index === this.currentSubmenuActive && document.getElementsByClassName('navbar-submenu')[index]) {
       const height = document.getElementsByClassName('navbar-submenu')[index].clientHeight;
       return {'height': `${76 + height}px`}
     } else {
