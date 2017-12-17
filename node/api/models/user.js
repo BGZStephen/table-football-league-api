e56203a8aa2a28d00a063cc51086f6f73ed9b733
Stephen Wright <sjw948@gmail.com>
2017-12-17 19:34:18 +0000

    extended user model methods with fixture, team & statistics updaters

--- node/api/models/user.js
+++ node/api/models/user.js
@@ -24,4 +24,58 @@ const UserSchema = Schema({
   leagues: [{type: Schema.ObjectId, ref: 'League'}],
 })
 
+UserSchema.methods = {
+  async updateStatistics(params) {
+    if (params.win) {
+      this.statistics.wins += 1;
+    }
+
+    if (params.loss) {
+      this.statistics.loss += 1;
+    }
+  },
+
+  async updateFixtures(params) {
+    params.add.forEach(function (fixture) {
+      if (this.fixtures.indexOf(fixture) === -1) {
+        this.fixtures.push(fixture);
+      }
+    })
+
+    params.remove.forEach(function (fixture) {
+      if (this.fixtures.indexOf(fixture) >= 0) {
+        this.fixtures.splice(this.fixtures.indexOf(fixture), 1);
+      }
+    })
+  },
+
+  async updateLeagues(params) {
+    params.add.forEach(function (league) {
+      if (this.leagues.indexOf(league) === -1) {
+        this.leagues.push(league);
+      }
+    })
+
+    params.remove.forEach(function (league) {
+      if (this.leagues.indexOf(league) >= 0) {
+        this.leagues.splice(this.leagues.indexOf(league), 1);
+      }
+    })
+  },
+
+  async updateTeams(params) {
+    params.add.forEach(function (team) {
+      if (this.teams.indexOf(team) === -1) {
+        this.teams.push(team);
+      }
+    })
+
+    params.remove.forEach(function (team) {
+      if (this.teams.indexOf(team) >= 0) {
+        this.teams.splice(this.teams.indexOf(team), 1);
+      }
+    })
+  }
+}
+
 module.exports = mongoose.model('User', UserSchema);
