e79b9b156775ccba4d1f0767dd4826d60c64b09e
Stephen Wright <sjw948@gmail.com>
2017-12-27 10:00:58 +0000

    updated validation to allow for basic type checking

--- api/services/validate.js
+++ api/services/validate.js
@@ -3,13 +3,31 @@
  * error messages if any keys are missing.
  * @param {Object} object object to validate existance of keys on.
  * @param {Object} params an object representation of expected keys whos values represent
- *                        an error message to send if key is missing on .
+ *                        an error message to send if key is missing on.
+ * @param {String} params.message error message to return if validation fails.
+ * @param {String} params.type expected object value type.
  */
-function validate(objectToValidate, validationParams) {
+function validateObject(object, params) {
   const errorArray = [];
-  Object.keys(validationParams).forEach(function(param) {
-    if(!objectToValidate[param]) {
-      errorArray.push(params[param])
+  Object.keys(params).forEach(function(param) {
+    if(!object[param]) {
+      return errorArray.push(params[param])
+    }
+
+    if (param.type === 'string' && typeof object[param] !== 'string') {
+      return errorArray.push(params[param])
+    }
+
+    if (param.type === 'number' && typeof value !== 'number') {
+      return errorArray.push(params[param])
+    }
+
+    if (param.type === 'object' && typeof value !== 'object') {
+      return errorArray.push(params[param])
+    }
+
+    if (param.type === 'array' && (typeof value !== 'object' && value.constructor !== Array)) {
+      return errorArray.push(params[param])
     }
   })
 
