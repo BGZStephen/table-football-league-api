2ed190f02092f1b4cd040f72d88ae66da8613afd
stephen <sjw948@gmail.com>
2019-05-11 08:48:13 +0100

    move user creation endpoint to domain helper instanciation

--- src/routes/public/users.ts
+++ src/routes/public/users.ts
@@ -1,44 +1,22 @@
 import { Request, Response, Router } from "express";
-import * as jwt from '../../utils/jwt';
-import * as validate from 'validate.js';
 import * as rest from '../../utils/rest';
-import { UserModel } from "../../models/user";
+import { User } from "../../domain/user/user";
 
 const router = Router();
 
 async function create(req: Request, res: Response): Promise<void> {
-  const validatorErrors = validate(req.body, {
-    email: {presence: {message: 'Email address is required'}},
-    firstName: {presence: {message: 'First name is required'}},
-    lastName: {presence: {message: 'Last name is required'}},
-    password: {presence: {message: 'Email address is required'}}
-  }, {format: "flat"})
-
-  if (validatorErrors) {
-    return res.error({message: validatorErrors, statusCode: 403});
-  }
-
-  const existingUser = await UserModel.findOne({email: req.body.email})
-
-  if (existingUser) {
-    return res.error({message: 'Email address already in use', statusCode: 400});
-  }
-
-  const user = await UserModel.create({
+  const user = await User.create({
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
-    username: req.body.username,
     password: req.body.password,
   })
 
-  const token = jwt.generateUserToken(user);
+  const token = user.generateJwt();
 
   res.json({
     token: token,
-    user: {
-      _id: user._id,
-    }
+    user: user.getPublicFields(),
   });
 }
 
