785d15b254aa7d193d353e44127404971270d2c2
stephen <sjw948@gmail.com>
2019-05-11 08:29:39 +0100

    user validation helpers

---
+++ src/domain/user/validator.ts
@@ -0,0 +1,26 @@
+import * as joi from 'joi';
+import * as joiUtils from '../../utils/joi';
+
+class UserValidatorService {
+  public validateNewUser(params) {
+    const schema = joi.object().keys({
+      firstName: joi.string().alphanum().required().label('First name'),
+      lastName: joi.string().alphanum().required().label('Last name'),
+      email: joi.string().email({ minDomainAtoms: 2 }).label('Email'),
+      password: joi.string().required().label('Password'),
+    });
+  
+    joiUtils.validateThrow(params, schema);
+  }
+
+  public validateAuthenticationCredentials(params) {
+    const schema = joi.object().keys({
+      email: joi.string().email().required().label('Email'),
+      password: joi.string().required().label('Password'),
+    });
+  
+    joiUtils.validateThrow(params, schema);
+  }
+}
+
+export const UserValidator = new UserValidatorService();
