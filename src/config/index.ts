5c83a06cb518bb20548c07180ce2f2299ab9217b
stephen <sjw948@gmail.com>
2019-05-12 10:03:07 +0100

    add typings for configurations

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,73 @@
-const configs = {
+interface IConfigBase {
+  env: string;
+  database: string;
+  jwtSecret: string;
+  dashboardUrl: string;
+  mail: {
+    defaults: {
+      from: string;
+      name: string;
+    }
+  }
+  mailjet: {
+    apiKey: string;
+    apiSecret: string;
+  }
+}
+
+interface IDevelopmentConfig extends IConfigBase {};
+interface IStagingConfig extends IConfigBase {};
+interface IProductionConfig extends IConfigBase {};
+
+interface IConfigs {
+  development: IDevelopmentConfig;
+  staging: IStagingConfig;
+  production: IProductionConfig;
+}
+
+const configs: IConfigs = {
   development: {
-    env: process.env.NODE_ENV,
+    env: 'development',
     database: 'mongodb://localhost/mytablefootball',
     jwtSecret: 'testing',
     dashboardUrl: 'http://localhost:9000',
     mail: {
-      defaultFrom: 'sjw948@gmail.com',
-      defaultName: 'The MyTableFootball Team',
+      defaults: {
+        from: 'sjw948@gmail.com',
+        name: 'The MyTableFootball Team',
+      }
+    },
+    mailjet: {
+      apiKey: '18b36c0843618ae057cb4a444f30297e',
+      apiSecret: '552a5cd2473ca1bbe6f7c9f0d716e77f'
+    }
+  },
+  staging: {
+    env: 'staging',
+    database: 'mongodb://localhost/mytablefootball',
+    jwtSecret: 'testing',
+    dashboardUrl: 'http://localhost:9000',
+    mail: {
+      defaults: {
+        from: 'sjw948@gmail.com',
+        name: 'The MyTableFootball Team',
+      }
+    },
+    mailjet: {
+      apiKey: '18b36c0843618ae057cb4a444f30297e',
+      apiSecret: '552a5cd2473ca1bbe6f7c9f0d716e77f'
+    }
+  },
+  production: {
+    env: 'production',
+    database: 'mongodb://localhost/mytablefootball',
+    jwtSecret: 'testing',
+    dashboardUrl: 'http://localhost:9000',
+    mail: {
+      defaults: {
+        from: 'sjw948@gmail.com',
+        name: 'The MyTableFootball Team',
+      }
     },
     mailjet: {
       apiKey: '18b36c0843618ae057cb4a444f30297e',
