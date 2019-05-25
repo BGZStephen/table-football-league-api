e864a7c4c633f6d303b333fcdde74826296e9854
stephen <sjw948@gmail.com>
2019-05-25 10:02:37 +0100

    user list validation

--- src/domain/user/validator.ts
+++ src/domain/user/validator.ts
@@ -1,6 +1,6 @@
 import * as joi from 'joi';
 import * as joiUtils from '../../utils/joi';
-import { IUserCreateParams, IUserAuthenticationParams } from './user';
+import { IUserCreateParams, IUserAuthenticationParams, IUserQuery } from './user';
 
 class UserValidatorService {
   public validateNewUser(params: IUserCreateParams) {
@@ -18,6 +18,15 @@ class UserValidatorService {
     const schema = joi.object().keys({
       email: joi.string().email().required().label('Email'),
       password: joi.string().required().label('Password'),
+    });
+  
+    joiUtils.validateThrow(params, schema);
+  }
+
+  public validateListQuery(params: IUserQuery) {
+    const schema = joi.object().keys({
+      _id: joi.alternatives().try(joi.array().items(joi.string().alphanum()), joi.string().alphanum()).label('User IDs'),
+      email: joi.alternatives().try(joi.array().items(joi.string().alphanum()), joi.string().alphanum()).label('Email address'),
     });
   
     joiUtils.validateThrow(params, schema);
