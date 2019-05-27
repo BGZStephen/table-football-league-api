a13fd015e99749bac6ed25ff0c3bbc4ce3244dd2
stephen <sjw948@gmail.com>
2019-05-27 08:42:04 +0100

    export team joi constraints for re-use

--- src/domain/team/validator.ts
+++ src/domain/team/validator.ts
@@ -4,23 +4,26 @@ import { ITeamCreateParams, ITeamQuery } from './team';
 
 class TeamValidatorService {
   public validateNewTeam(params: ITeamCreateParams) {
-    const schema = joi.object().keys({
-      name: joi.string().alphanum().required().label('Team Name'),
-      userIds: joi.array().items(joi.string().alphanum()).label('User IDs'),
-    });
-  
+    const schema = joi.object().keys(newTeamConstraint);
     joiUtils.validateThrow(params, schema);
   }
 
   public validateListQuery(params: ITeamQuery) {
-    const schema = joi.object().keys({
-      name: joi.string().alphanum().label('Team Name'),
-      _id: joi.string().label('Team IDs'),
-      userId: joi.string().label('User IDs'),
-    });
+    const schema = joi.object().keys(teamListConstraint);
   
     joiUtils.validateThrow(params, schema);
   }
 }
 
 export const TeamValidator = new TeamValidatorService();
+
+export const newTeamConstraint = {
+  name: joi.string().alphanum().required().label('Team Name'),
+  userIds: joi.array().items(joi.string().alphanum()).label('User IDs'),
+}
+
+export const teamListConstraint = {
+  name: joi.string().alphanum().label('Team Name'),
+  _id: joi.string().label('Team IDs'),
+  userId: joi.string().label('User IDs'),
+}
