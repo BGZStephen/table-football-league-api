8a38b49dd7a39e618a46b5f3d78f82569591bb19
stephen <sjw948@gmail.com>
2019-05-29 10:25:59 +0100

    message mongoose model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 require('./user');
 require('./team');
 require('./game');
+require('./message');
---
+++ src/models/message.ts
@@ -0,0 +1,40 @@
+import { Schema, Model, model, Document } from 'mongoose';
+import * as _ from 'lodash';
+import { IUser } from './user';
+
+export interface IMessage extends Document {
+  _id: string;
+  createdOn: Date;
+  viewedOn: Date;
+  createdBy: string;
+  senderId: string;
+  receipientId: string;
+  sender: IUser;
+  recipient: IUser;
+  content: string;
+  title: string;
+}
+
+const MessageSchema = new Schema({
+  createdOn: {type: Date, required: true, default: () => new Date()},
+  viewedOn: {type: Date, required: true, default: () => new Date()},
+  createdBy: {type: String},
+  senderId: {type: String},
+  receipientId: {type: String},
+  title: {type: String},
+  content: {type: String},
+})
+
+MessageSchema.virtual('sender', {
+  ref: 'User',
+  localField: 'senderId',
+  foreignField: '_id',
+});
+
+MessageSchema.virtual('recipient', {
+  ref: 'User',
+  localField: 'recipientID',
+  foreignField: '_id',
+});
+
+export const MessageModel: Model<IMessage> = model<IMessage>('Message', MessageSchema);
