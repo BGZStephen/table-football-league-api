6a2e7adbfeb6ad8c9f718e559db7a62e7f28c41f
stephen <sjw948@gmail.com>
2019-05-30 09:06:03 +0100

    fix TS HTTPError failing transpile

--- src/utils/error-utils.ts
+++ src/utils/error-utils.ts
@@ -1,11 +1,11 @@
 import { config } from '../config'
 import { Request, Response, NextFunction } from "express";
 import * as Debug from "debug";
-import { HTTPError } from '../domain/errors/http-error';
+import { HTTPErrorInstance } from '../domain/errors/http-error';
 
 const debug = Debug("api")
 
-export function logErrors (err: HTTPError, req: Request, res: Response, next: NextFunction) {
+export function logErrors (err: HTTPErrorInstance, req: Request, res: Response, next: NextFunction) {
   if (config.env === 'development') {
     debug(err);
   }
@@ -14,7 +14,7 @@ export function logErrors (err: HTTPError, req: Request, res: Response, next: Ne
 }
 
 // handle all express generated errors with stacks
-export function errorHandler (err: HTTPError, req: Request, res: Response) {
+export function errorHandler (err: HTTPErrorInstance, req: Request, res: Response) {
   res.statusMessage = err.message || "Something went wrong";
   return res.status(err.statusCode || 500).send(err.message);
 }
