fd11f1f4797ca886fe6f117f9a6dc6bc4e227fd2
stephen <sjw948@gmail.com>
2019-06-01 13:39:05 +0100

    user set as part of middleware

--- src/routes/public/middleware.ts
+++ src/routes/public/middleware.ts
@@ -1,7 +1,30 @@
 import { Request, Response, NextFunction } from "express";
+import * as jwt from "../../utils/jwt";
+import { ObjectId } from "bson";
+import { UserModel } from "../../models/user";
+import { HTTPError } from "../../domain/errors/http-error";
+import { UserService } from "../../domain/user/service";
 
 export async function setUserContext(req: Request, res: Response, next: NextFunction) {
-  if (!req.header("x-access-token")) {
+  if (!req.headers["x-access-token"]) {
     return next();
   }
+
+  const accessTokenContent = await jwt.decodeToken(req.headers["x-access-token"] as string)
+
+  const userId = accessTokenContent.id;
+
+  if (!userId || typeof userId !== "string" || !ObjectId.isValid(userId)) {
+    throw new Error("Invalud User ID")
+  }
+
+  const user = await UserModel.findById(new ObjectId(userId));
+
+  if (!user) {
+    throw HTTPError("User not found", 404);
+  }
+
+  UserService.set(user);
+
+  next();
 }
